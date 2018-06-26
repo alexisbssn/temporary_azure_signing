e396f88c09eff762723885dd73e1659b4c02ee8b
alexisbssn <alexis.buisson@hotmail.com>
2018-06-26 15:23:34 -0400

    Add project files.

---
+++ AzureHsmPOC/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AzureHsmPOC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
